50c3562f429471a92d3c41d5c377dc5b7535c676
oskarfahlstrom <hej@oskarfahlstrom.com>
2024-10-06 11:23:24 +0200

    cleanup using std::clamp

--- rgb-to-hex-conversion/rgb-to-hex.cpp
+++ rgb-to-hex-conversion/rgb-to-hex.cpp
@@ -5,8 +5,8 @@
 std::string rgb_to_hex(int r, int g, int b)
 {  
     std::stringstream stream;
-    stream << std::setfill ('0') << std::setw(2) << std::hex << std::max(0, std::min(255, r));
-    stream << std::setfill ('0') << std::setw(2) << std::hex << std::max(0, std::min(255, g));
-    stream << std::setfill ('0') << std::setw(2) << std::hex << std::max(0, std::min(255, b));
+    stream << std::setfill ('0') << std::setw(2) << std::hex << std::clamp(r, 0, 255);
+    stream << std::setfill ('0') << std::setw(2) << std::hex << std::clamp(g, 0, 255);
+    stream << std::setfill ('0') << std::setw(2) << std::hex << std::clamp(b, 0, 255);
     return stream.str();
 }
