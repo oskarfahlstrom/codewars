97d99a6653ddb8eb73d31729f9fb327db839fd7b
oskarfahlstrom <hej@oskarfahlstrom.com>
2024-10-10 12:00:37 +0200

    neater solution using auto

--- move-zeros-to-the-end/move_zeroes.cpp
+++ move-zeros-to-the-end/move_zeroes.cpp
@@ -6,3 +6,8 @@ std::vector<int> move_zeroes(const std::vector<int>& input) {
   std::stable_partition(result.begin(), result.end(), [](int x){ return x != 0; });
   return result;
 }
+
+auto move_zeroes_neato(std::vector<int> v) {
+  stable_partition(begin(v), end(v), std::negate());
+  return v;
+}
