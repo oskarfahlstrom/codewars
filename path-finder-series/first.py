70885709cfac7a490655fe6f0c0fb287b4997785
oskarfahlstrom <hej@oskarfahlstrom.com>
2024-10-31 15:40:27 +0100

    :zap: refactored and made more efficient

--- path-finder-series/first.py
+++ path-finder-series/first.py
@@ -1,11 +1,6 @@
-DIRECTIONS = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-
 def string_to_matrix(s: str):
     """Convert the string representation to a 2D matrix."""
     return list(map(list, s.splitlines()))
-
-
 
 def path_finder(s: str):
     """Perform search to check if there's a path from (0, 0) to (n-1, n-1)."""
@@ -13,19 +8,18 @@ def path_finder(s: str):
     n = len(matrix)
     
     while queue:
-        x, y = queue.pop()  # swap to pop(0) to use DFS instead of BFS
+        x, y = queue.pop()  # swap to pop(0) to use BFS instead of DFS
         matrix[x][y] = 'X'  # mark as visited
         
         if (x, y) == (n-1, n-1):
             return True
         
-        for dx, dy in DIRECTIONS:
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < n and 0 <= ny < n and (nx, ny) and matrix[nx][ny] == '.':
                 queue.append((nx, ny))
     
     return False
-
 
 if __name__ == "__main__":
     assert path_finder("\n".join([".W.", ".W.", "..."])) == True
