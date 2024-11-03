c6a624f2fc53529a0bcecbef879b7fdf3e90e072
oskarfahlstrom <hej@oskarfahlstrom.com>
2024-11-03 21:13:18 +0100

    :construction: added first solution for fourth level, passes initial tests

---
+++ path-finder-series/4.py
@@ -0,0 +1,11 @@
+import re
+
+def i_am_here(path: str):
+    p = re.sub(r'([a-zA-Z])(\d*)', lambda x: x[1] * int(x[2] or 1), path)
+    return [(p.count('l') - p.count('R')), (p.count('r') - p.count('L'))]
+
+
+if __name__ == "__main__":
+    assert i_am_here('') == [0, 0]
+    assert i_am_here('RLrl') == [0, 0]
+    assert i_am_here('r5L2l4') == [4, 3]
