f7873b99b203e2788c4afead1f9c9dd587894f44
oskarfahlstrom <hej@oskarfahlstrom.com>
2025-01-03 16:33:19 +0100

    :zap: more concise solution for 'Hello, Name or World!'

--- hello-name-or-world/hello.py
+++ hello-name-or-world/hello.py
@@ -1,5 +1,5 @@
 """https://www.codewars.com/kata/57e3f79c9cb119374600046b"""
 
 def hello(name: str = ''):
-    return f"Hello, {'World' if not name else name.lower().capitalize()}!"
+    return f"Hello, {name.lower().capitalize() or 'World'}!"
     
